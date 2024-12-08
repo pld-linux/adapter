6fc266cdd4e383ea638b3ce19a1defdca005824f
Jan Rękorajski <baggins@pld-linux.org>
2024-12-08 23:10:24 +0100

    - 1.519 everywhere

--- adapter.sh
+++ adapter.sh
@@ -12,7 +12,7 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-VERSION="1.517"
+VERSION="1.519"
 VERSIONSTRING="\
 Adapter adapts .spec files for PLD Linux.
 v$VERSION (C) 1999-2021 Free Penguins".
